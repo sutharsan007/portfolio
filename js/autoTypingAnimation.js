1ede7265a46c08bd51e6ab78a3fba042cc327d65
sutharsan007 <100124033+sutharsan007@users.noreply.github.com>
2023-03-29 09:22:46 +0530

    initial commit

---
+++ js/autoTypingAnimation.js
@@ -0,0 +1,10 @@
+setTimeout(function() {
+    $('.about-info-inner').typed({
+      strings: [
+        "<span>A talented Software Engineer aspiring to gain an entry level position in a reputed company. Gained extensive experience in  software development, testing, and deployment through internships. A highly motivated and hardworking individual skilled in C/C++ java and other technical problem solving . Seeking a position to utilize the gained skills and experience to positively contribute to the company and easily adaptable in all work."
+        
+      ],
+      typeSpeed: 7,
+      contentType: 'html'
+    });
+  }, 100);
